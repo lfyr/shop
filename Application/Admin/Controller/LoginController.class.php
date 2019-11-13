d2c2a0c1907093c0decf22721d52899a5629381d
lfyr <976258825@qq.com>
2019-11-13 16:46:41 +0800

    调整配置

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -1,46 +1,48 @@
 <?php
 namespace Admin\Controller;
+
 use Think\Controller;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
 
-    public function login(){
-
-      if (IS_POST) {
-        $model=D("Admin");
-        if($model->validate($model->_login_validate)->create()){
-          if(TRUE===$model->login()){
-            redirect(U('Admin/Index/index')); //直接跳转
-          }
+    public function login()
+    {
+        if (IS_POST) {
+            $model = D("Admin");
+            if ($model->validate($model->_login_validate)->create()) {
+                if (true === $model->login()) {
+                    redirect(U('Admin/Index/index')); //直接跳转
+                }
+            }
+            $this->error($model->getError());
         }
-        $this->error($model->getError());
-      }
-      $this->display();
+        $this->display();
     }
 
-    public function chkcode(){
+    public function chkcode()
+    {
 
-      $Verify = new \Think\Verify(array(
-        'length' => 3,
-        'useNoise' => false,
-        'imageW' => 80,
-        'imageH' => 32,
-        'fontSize' => 15,
-      ));
-    $Verify->entry();
+        $Verify = new \Think\Verify(array(
+            'length'   => 3,
+            'useNoise' => false,
+            'imageW'   => 80,
+            'imageH'   => 32,
+            'fontSize' => 15,
+        ));
+        $Verify->entry();
     }
 
+    public function logout()
+    {
 
-    public function logout(){
+        $_SESSION = array(); //清除SESSION值
 
-      $_SESSION=array(); //清除SESSION值
-
-      if(isset($_COOKIE[session_name()])){
-        setcookie(session_name(),'',time()-1,'/');
-      }
-      session_destroy();  //清除服务器的sesion文件
-      redirect(U('Admin/Login/login')); //直接跳转
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time() - 1, '/');
+        }
+        session_destroy(); //清除服务器的sesion文件
+        redirect(U('Admin/Login/login')); //直接跳转
     }
 }
 // file_put_contents('./axt.txt', var_export($data, TRUE));
-
